3e1a7fa45f9cd639fc4063899c6c2b6516fe0733
Jarronb <baileyjarron@gmail.com>
2018-02-17 22:42:38 -0600

    Changed file name of congratulations

--- site.js
+++ site.js
@@ -1,4 +1,3 @@
-<<<<<<< current
 $.noConflict();
 (function($) {
   $('#signup-form').on('submit', function(e) {
@@ -38,36 +37,7 @@ $.noConflict();
     if ((valid.email === true) && (valid.name === true)) {
       $('#header.para').replaceWith('<p>Thank you ' + $('#name').val() + ', for signing up.' +
       'Please confirm your email ' + email.val + '.</p>');
-      window.location.href = "congratulations.html";
+      window.location.href = "congratulations/index.html";
     }
-
-
-
 });
 })(jQuery);
-=======
-$.noConflict();
-(function($) {
-
-
-  $('#signup-form').on('submit', function(e) {
-    e.preventDefault(); //No id value in url
-    var x = new Date(document.getElementById('dob').value); //get person's dob
-    var dob = x.getFullYear();
-    var current_date = new Date(); //get current date
-    var today = current_date.getFullYear(); //get the current year
-    var age = today - dob; //subtract the peron's dob from current year
-    console.log(age);
-
-    if(age < 18) {
-      $('#form-item-dob').toggleClass('age-warning');
-      $('#form-item-dob').prepend('<span class="warning">You need to be 18 years old or older.</span>');
-      $('#dob').css('background', '#D24D57')
-    }
-    if (age >= 18) {
-      $('#signup-form').append('Form Submitted Succesfully');
-    }
-  });
-});
-})(jQuery);
->>>>>>> before discard
